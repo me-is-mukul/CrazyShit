38d0664aa0953279e33ad9030b5f5a46453fe215
me-is-mukul <me.is.mukul.2005@gmail.com>
2025-07-17 17:49:22 +0530

    progress bar

--- js/main.js
+++ js/main.js
@@ -3,3 +3,13 @@ var typed = new Typed('#element', {
     typeSpeed: 160,
     loop: true
 });
+
+
+window.addEventListener('scroll', () => {
+    const scrollTop = window.scrollY;
+    const docHeight = document.body.scrollHeight - window.innerHeight;
+    const scrollPercent = (scrollTop / docHeight) * 100;
+
+    const progressBar = document.querySelector('.scroll-progress');
+    progressBar.style.width = scrollPercent + '%';
+  });
