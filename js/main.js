56f85ab5b3587c9155dbb031aa41b937d3b8a527
me-is-mukul <me.is.mukul.2005@gmail.com>
2025-07-17 20:56:39 +0530

    aah

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 var typed = new Typed('#element', {
     strings: ['Web developer', 'Blockchain learner', 'Cyber security learner', 'Game Developer', 'Digital Designer', 'Chess Enthusiast', 'Flutist'],
     typeSpeed: 160,
-    loop: true
+    loop: true,
+    showCursor:false
 });
 
 
