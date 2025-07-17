764e4403a9c1f08864539a8070657b9d94aa1642
me-is-mukul <me.is.mukul.2005@gmail.com>
2025-07-17 16:10:34 +0530

    initial commit

---
+++ js/header.js
@@ -0,0 +1,39 @@
+
+new FinisherHeader({
+    "count": 9,
+    "size": {
+        "min": 1227,
+        "max": 1500,
+        "pulse": 0
+    },
+    "speed": {
+        "x": {
+            "min": 0.1,
+            "max": 0.4
+        },
+        "y": {
+            "min": 0.1,
+            "max": 0.4
+        }
+    },
+    "colors": {
+        "background": "#120f15ff",
+        "particles": [
+            "#240046",
+            "#412658ff",
+            "#5a189a",
+            "#3c096c",
+            "#240046"
+        ]
+    },
+    "blending": "overlay",
+    "opacity": {
+        "center": 0.55,
+        "edge": 0.05
+    },
+    "skew": -2.2,
+    "shapes": [
+        "c",
+        "c"
+    ]
+});
